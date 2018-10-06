56262236532c844b3fe1977dde71041f79d12f26
Ben Smith <binji@chromium.org>
2018-10-05 17:25:51 -0700

    More tests

--- src/test-wasm-api.cc
+++ src/test-wasm-api.cc
@@ -73,7 +73,22 @@ TEST(WasmApi, FuncType) {
                  ValKindInitializerList expected_results) {
     auto ft = FuncType::make(MakeVecValType(expected_params),
                              MakeVecValType(expected_results));
+    const auto& cft = ft;
 
+    // Test ExternType::*.
+    EXPECT_EQ(EXTERN_FUNC, ft->kind());
+    EXPECT_EQ(ft.get(), ft->func());
+    EXPECT_EQ(nullptr, ft->global());
+    EXPECT_EQ(nullptr, ft->table());
+    EXPECT_EQ(nullptr, ft->memory());
+    EXPECT_EQ(cft.get(), cft->func());
+    EXPECT_EQ(nullptr, cft->global());
+    EXPECT_EQ(nullptr, cft->table());
+    EXPECT_EQ(nullptr, cft->memory());
+
+    // TODO ExternType::copy.
+
+    // Test FuncType::params().
     {
       const auto& actual_params = ft->params();
       EXPECT_EQ(expected_params.size(), actual_params.size());
@@ -84,6 +99,7 @@ TEST(WasmApi, FuncType) {
       }
     }
 
+    // Test FuncType::results().
     {
       const auto& actual_results = ft->results();
       EXPECT_EQ(expected_results.size(), actual_results.size());
@@ -120,4 +136,111 @@ TEST(WasmApi, FuncType) {
       }
     }
   }
+}
+
+TEST(WasmApi, GlobalType) {
+  auto test = [](ValKind kind, Mutability mut) {
+    auto gt = GlobalType::make(ValType::make(kind), mut);
+    const auto& cgt = gt;
+
+    // Test ExternType::*.
+    EXPECT_EQ(EXTERN_GLOBAL, gt->kind());
+    EXPECT_EQ(nullptr, gt->func());
+    EXPECT_EQ(gt.get(), gt->global());
+    EXPECT_EQ(nullptr, gt->table());
+    EXPECT_EQ(nullptr, gt->memory());
+    EXPECT_EQ(nullptr, cgt->func());
+    EXPECT_EQ(cgt.get(), cgt->global());
+    EXPECT_EQ(nullptr, cgt->table());
+    EXPECT_EQ(nullptr, cgt->memory());
+
+    // Test GlobalType::*.
+    EXPECT_EQ(kind, gt->content()->kind());
+    EXPECT_EQ(mut, gt->mutability());
+  };
+
+  for (auto mut: {CONST, VAR}) {
+    for (auto kind : {I32, I64, F32, F64, ANYREF, FUNCREF}) {
+      test(kind, mut);
+    }
+  }
+}
+
+TEST(WasmApi, TableType) {
+  auto test = [](ValKind element_kind, Limits limits) {
+    auto tt = TableType::make(ValType::make(element_kind), limits);
+    const auto& ctt = tt;
+
+    // Test ExternType::*.
+    EXPECT_EQ(EXTERN_TABLE, tt->kind());
+    EXPECT_EQ(nullptr, tt->func());
+    EXPECT_EQ(nullptr, tt->global());
+    EXPECT_EQ(tt.get(), tt->table());
+    EXPECT_EQ(nullptr, tt->memory());
+    EXPECT_EQ(nullptr, ctt->func());
+    EXPECT_EQ(nullptr, ctt->global());
+    EXPECT_EQ(ctt.get(), ctt->table());
+    EXPECT_EQ(nullptr, ctt->memory());
+
+    // Test TableType::*.
+    EXPECT_EQ(element_kind, tt->element()->kind());
+    EXPECT_EQ(limits.min, tt->limits().min);
+    EXPECT_EQ(limits.max, tt->limits().max);
+  };
+
+  for (auto limits: {Limits(0), Limits(1), Limits(2, 3)}) {
+    for (auto element_kind : {I32, I64, F32, F64, ANYREF, FUNCREF}) {
+      test(element_kind, limits);
+    }
+  }
+}
+
+TEST(WasmApi, MemoryType) {
+  auto test = [](Limits limits) {
+    auto mt = MemoryType::make(limits);
+    const auto& cmt = mt;
+
+    // Test ExternType::*.
+    EXPECT_EQ(EXTERN_MEMORY, mt->kind());
+    EXPECT_EQ(nullptr, mt->func());
+    EXPECT_EQ(nullptr, mt->global());
+    EXPECT_EQ(nullptr, mt->table());
+    EXPECT_EQ(mt.get(), mt->memory());
+    EXPECT_EQ(nullptr, cmt->func());
+    EXPECT_EQ(nullptr, cmt->global());
+    EXPECT_EQ(nullptr, cmt->table());
+    EXPECT_EQ(cmt.get(), cmt->memory());
+
+    // Test MemoryType::*.
+    EXPECT_EQ(limits.min, mt->limits().min);
+    EXPECT_EQ(limits.max, mt->limits().max);
+  };
+
+  for (auto limits: {Limits(0), Limits(1), Limits(2, 3)}) {
+    test(limits);
+  }
+}
+
+Name MakeName(const char* name) {
+  // TODO Name::make(std::string) will append an additional \0
+  return Name::make(std::string(name));
+}
+
+TEST(WasmApi, ImportType) {
+  auto test = [](const char* module, const char* name, const ExternType& type) {
+    auto it = ImportType::make(MakeName(module), MakeName(name), type.copy());
+
+    EXPECT_STREQ(module, it->module().get());
+    EXPECT_STREQ(name, it->name().get());
+  };
+
+  auto ft = FuncType::make(MakeVecValType({}), MakeVecValType({}));
+  auto gt = GlobalType::make(ValType::make(I32), CONST);
+  auto tt = TableType::make(ValType::make(ANYREF), Limits(3, 5));
+  auto mt = MemoryType::make(Limits(0, 4));
+
+  test("mod", "func", *ft);
+  test("mod", "global", *gt);
+  test("mod", "table", *tt);
+  test("mod", "memory", *mt);
 }
